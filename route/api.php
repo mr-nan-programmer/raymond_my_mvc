f14ae30637970f50d4e7db8d2f8825cfd1775bdb
Mr_NaN <daniel.golnari6@gmail.com>
2024-11-29 00:38:33 +0330

    test github update

--- route/api.php
+++ route/api.php
@@ -1,6 +1,9 @@
 <?php
 
 use App\Http\Controllers\api;
+use MrNan\Main\Router;
 
 Router::call_page("/githubupdate",[api::class,"index"],[api::class,"index"]);
+Router::call_page("/githubupdate2",[\App\Http\Controllers\home_controllers::class,"index"],[api::class,"index"]);
+
  ?>
