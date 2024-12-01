f988b48d21574dbb0050ac9277d84d45274204ba
Mr_NaN <daniel.golnari6@gmail.com>
2024-12-01 17:15:36 +0330

    contact en zh

--- resources/views/5page/contact.blade.php
+++ resources/views/5page/contact.blade.php
@@ -1,5 +1,5 @@
 @extends('layout.route')
-@section("title","contact")
+@section("title","联系我们 | Raymond")
 
 @section('content')
 
@@ -7,7 +7,7 @@
     <div class="modal fade bg-white" id="templatemo_search" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog modal-lg" role="document">
             <div class="w-100 pt-1 mb-5 text-right">
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="关闭"></button>
             </div>
             <form action="" method="get" class="modal-content modal-body border-0 p-0">
                 <div class="input-group mb-2">
@@ -20,52 +20,53 @@
         </div>
     </div>
 
-    <!-- 开始内容页面 -->
+    <!-- 页面内容开始 -->
     <div class="container-fluid bg-light py-5">
         <div class="col-md-6 m-auto text-center">
             <h1 class="h1">联系我们</h1>
             <p>
-                Proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                Lorem ipsum dolor sit amet.
+                选择您想联系的部门。
             </p>
         </div>
     </div>
 
-    <!-- 开始地图 -->
-    <div id="mapid" style="width: 100%; height: 300px;"></div>
-    <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js" integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA==" crossorigin=""></script>
-    <!-- 结束地图 -->
+    <!-- 地图开始 -->
 
-    <!-- 开始联系 -->
-    <div class="container py-5">
-        <div class="row py-5">
-            <form class="col-md-9 m-auto" method="post" role="form">
-                <div class="row">
-                    <div class="form-group col-md-6 mb-3">
-                        <label for="inputname">姓名</label>
-                        <input type="text" class="form-control mt-1" id="name" name="name" placeholder="姓名">
-                    </div>
-                    <div class="form-group col-md-6 mb-3">
-                        <label for="inputemail">邮箱</label>
-                        <input type="email" class="form-control mt-1" id="email" name="email" placeholder="邮箱">
-                    </div>
-                </div>
-                <div class="mb-3">
-                    <label for="inputsubject">主题</label>
-                    <input type="text" class="form-control mt-1" id="subject" name="subject" placeholder="主题">
-                </div>
-                <div class="mb-3">
-                    <label for="inputmessage">信息</label>
-                    <textarea class="form-control mt-1" id="message" name="message" placeholder="信息" rows="8"></textarea>
-                </div>
-                <div class="row">
-                    <div class="col text-end mt-2">
-                        <button type="submit" class="btn btn-success btn-lg px-3">谈一谈</button>
-                    </div>
-                </div>
-            </form>
+    <!-- 地图部分 -->
+
+    <!-- 联系部分开始 -->
+
+    <!-- 联系部分结束 -->
+    <section class="container py-5">
+        <div class="row text-center pt-5 pb-3">
+            <div class="col-lg-6 m-auto">
+                <h1 class="h1">选择</h1>
+                <p>
+                    您想联系哪个部门？
+                </p>
+            </div>
         </div>
-    </div>
-    <!-- 结束联系 -->
+        <div class="row" style="; justify-content: space-evenly;"  >
+            <div class="col-md-6 col-lg-3 pb-5" >
+                <div class="h-100 py-5 services-icon-wap shadow">
+                    <div class="h1 text-success text-center"><i class="fa-solid fa-comments"></i></div>
+                    <h2 class="h5 mt-4 text-center">反馈</h2>
+                </div>
+            </div>
+            <div class="col-md-6 col-lg-3 pb-5">
+                <div class="h-100 py-5 services-icon-wap shadow">
+                    <div class="h1 text-success text-center"><i class="fa-solid fa-cart-flatbed"></i></div>
+                    <h2 class="h5 mt-4 text-center">订单注册</h2>
+                </div>
+            </div>
+            <div class="col-md-6 col-lg-3 pb-5">
+                <div class="h-100 py-5 services-icon-wap shadow">
+                    <div class="h1 text-success text-center"><i class="fa fa-headset fa-lg"></i></div>
+                    <h2 class="h5 mt-4 text-center">支持</h2>
+                </div>
+            </div>
+        </div>
+    </section>
+
 
 @endsection
